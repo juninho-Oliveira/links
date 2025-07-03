f04364efd1075ceaea5cbfa0f32cbb9d5264bcca
junior <junior.oliveira05@icloud.com>
2025-07-03 16:46:36 -0300

    feat: add componente header

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from 'react-native';
+import { Image, View, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
+import { MaterialIcons } from "@expo/vector-icons"
+import { colors } from '@/styles/colors';
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Hello React Native!</Text>
-            <Text style={styles.title}>Olá</Text>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+                <TouchableOpacity >
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,15 +1,26 @@
-import {colors} from "../../styles/colors"
+import {colors} from "@/styles/colors"
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        paddingTop: 52,
+        
     },
     title: {
         color: colors.green[900],
         fontSize: 25,
+    },
+    header: {
+        paddingHorizontal: 24,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 32,
+    },
+    logo: {
+        height: 32,
+        width: 38,
     }
 })
