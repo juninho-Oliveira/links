d16fab86da61553f3b623d6dbbfee0e3ef9e8cd5
junior <junior.oliveira05@icloud.com>
2025-07-04 09:10:28 -0300

    feat: add componente Category e tipagem

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,7 @@ import { colors } from "@/styles/colors";
 
 export default function Layout() {
 
-    const backgroundColor = colors.gray[950]
+    const backgroundColor = colors.gray[950] /*Variavel que recebe a cor */
 
     return (
         <Stack screenOptions={{
--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,16 +3,23 @@ import { styles } from './styles';
 import { MaterialIcons } from "@expo/vector-icons"
 import { colors } from '@/styles/colors';
 
+import { Category } from '@/components/category';
+
+
 export default function Index() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-                <TouchableOpacity >
+                <TouchableOpacity > {/*elementos interativos, como botões, que respondem ao toque do usuário com um efeito de opacidade transparência */}
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+
+            <Category name="Projetos" icon="code" />
+            <Category name="Site" icon="language"/>
+            <Category name="video" icon="movie"/>
         </View>
     );
 }
